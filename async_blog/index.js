dd06f31a6d3ae1de80dc9a08a9e25c4307ad3aa9
Shan Batla <shan.batla@gmail.com>
2017-01-31 23:59:33 -0800

    Add variable declerations for user posts, albums, and todos.

--- async_blog/index.js
+++ async_blog/index.js
@@ -3,6 +3,10 @@ const postsUrl = 'https://jsonplaceholder.typicode.com/posts';
 const albumsUrl = 'https://jsonplaceholder.typicode.com/albums';
 const todosUrl = 'https://jsonplaceholder.typicode.com/todos';
 
+let numOfUserPosts = 0;
+let numOfUserAlbums = 0;
+let numOfUserTodos = 0;
+
 const initFetchGet = {
   method: 'GET'
 };

da5446ee46b160127dcf4dfe5f5696aa59f6207e
Shan Batla <shan.batla@gmail.com>
2017-01-31 23:55:05 -0800

    Add urls for posts, albums, and todos.

--- async_blog/index.js
+++ async_blog/index.js
@@ -1,4 +1,7 @@
 const urlUserPosts = 'https://jsonplaceholder.typicode.com/posts?userId=1';
+const postsUrl = 'https://jsonplaceholder.typicode.com/posts';
+const albumsUrl = 'https://jsonplaceholder.typicode.com/albums';
+const todosUrl = 'https://jsonplaceholder.typicode.com/todos';
 
 const initFetchGet = {
   method: 'GET'

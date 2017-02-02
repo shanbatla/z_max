6d0b4b6b0348c959ac8dce4b271b6bf8bfdf9281
Shan Batla <shan.batla@gmail.com>
2017-02-01 22:49:23 -0800

    Fetch user posts and populate console.

--- async_blog/index.js
+++ async_blog/index.js
@@ -2,16 +2,27 @@ const urlPosts = 'https://jsonplaceholder.typicode.com/posts?userId=1';
 const urlAlbums = 'https://jsonplaceholder.typicode.com/albums?userId=1';
 const urlTodos = 'https://jsonplaceholder.typicode.com/todos?userId=1';
 
-let numOfUserPosts = 0;
-let numOfUserAlbums = 0;
-let numOfUserTodos = 0;
+let numOfUserPosts;
+let numOfUserAlbums;
+let numOfUserTodos;
+let userPosts;
 
 const initFetchGet = {
   method: 'GET'
 };
 
-function userDesc(posts, albums, todos) {
-  console.log('Bret has ' + posts + ' posts, ' + albums + ' albums, and ' + todos + ' todos');
+function logUserDesc(numPosts, numAlbums, numTodos) {
+  console.log('Bret has ' + numPosts + ' posts, ' + numAlbums + ' albums, and ' + numTodos + ' todos');
+}
+
+function logUserPosts(posts) {
+  posts.forEach(function(post, index) {
+    if((index + 1) <= 5) {
+      console.log('Post ' + (index + 1) + ': ' + post.title);
+    } else {
+      return;
+    }
+  });
 }
 
 function fetchPosts() {
@@ -21,6 +32,7 @@ function fetchPosts() {
     })
     .then(function(posts) {
       numOfUserPosts = posts.length;
+      userPosts = posts;
       fetchAlbums();
     })
     .catch(function(error) {
@@ -49,7 +61,8 @@ function fetchTodos() {
     })
     .then(function(todos) {
       numOfUserTodos = todos.length;
-      userDesc(numOfUserPosts, numOfUserAlbums, numOfUserTodos);
+      logUserDesc(numOfUserPosts, numOfUserAlbums, numOfUserTodos);
+      logUserPosts(userPosts);
     })
     .catch(function(error) {
       console.log('Request failed: ' + error);
